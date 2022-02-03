9db7e218d0c7c55143fdeef5632d662048181de4
joamontesgi <joamontesgi@unal.edu.co>
2022-02-03 16:41:51 -0500

    microservicios

---
+++ app/Http/Controllers/ProductosController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Productos;
+
+class ProductosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $productos = Productos::all();
+        return  $productos;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $productos = new Productos();
+        $productos->nombre = $request->nombre;
+        $productos->descripcion = $request->descripcion;
+        $productos->precio = $request->precio;
+        $productos->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Productos::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,$id)
+    {
+        
+        $productos=Productos::find($id);
+        $productos->update($request->all());
+        return $productos;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy( Productos $producto)
+    {
+        $producto->delete();
+    }
+
+}
---
+++ app/Models/productos.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class productos extends Model
+{
+    use HasFactory;
+    protected $table = 'productos';
+    protected $primaryKey = 'id';
+    //filleable
+    protected $fillable = ['nombre','descripcion','precio'];
+}
